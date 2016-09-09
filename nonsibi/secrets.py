2299c92382d6a317344e12b388b08c5643669f13
David Conley <conleydg@gmail.com>
2016-09-09 19:29:44 -0400

    Initial commit

---
+++ nonsibi/secrets.py
@@ -0,0 +1,13 @@
+SECRET_KEY = "c_l1xcucr2)$1)^s+-_^b)c#b@8s(71)7nv6=#e$s%*9qb++0d"
+
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'nonsibi',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    }
+}
